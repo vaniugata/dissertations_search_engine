489a1bfbc5d5efa0d636d90a8c9add12b51532ad
vaniugata <vaniugata1988@gmail.com>
2020-01-20 22:43:33 +0200

    Upload script to read pdf and some dummy pdfs in dir "src"

---
+++ read_pdfs.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+import glob
+
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
+#read pdfs
+for file_path in glob.glob('src/*'):
+   print(file_path)
+   print(convert_pdf_to_txt(file_path))
+   
+
+    
+
